63be88e202f32be2b64999937f1bf0678477eed9
kramskoy <dkramskoy.13@gmail.com>
2019-01-14 09:45:33 +0300

    Updates

---
+++ precache-manifest.815f5e7fdce60c28326a20cda6d85e66.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "fac8ebeb719e45f449a8114c8bf54641",
+    "url": "/test-assignment/static/media/logo.fac8ebeb.svg"
+  },
+  {
+    "revision": "54d4cbd4d6008af26be0",
+    "url": "/test-assignment/static/js/runtime~main.54d4cbd4.js"
+  },
+  {
+    "revision": "13114b4ce836ac07c1b0",
+    "url": "/test-assignment/static/js/main.13114b4c.chunk.js"
+  },
+  {
+    "revision": "3c07b8417b75ed29b2fb",
+    "url": "/test-assignment/static/js/1.3c07b841.chunk.js"
+  },
+  {
+    "revision": "13114b4ce836ac07c1b0",
+    "url": "/test-assignment/static/css/main.d15dae53.chunk.css"
+  },
+  {
+    "revision": "adca7444861273a370900b9a7e248f15",
+    "url": "/test-assignment/index.html"
+  }
+];
